4897c2177eab2fde0284099595c8f3aada799eee
Tae.Garam (Home) <drasla217@gmail.com>
2026-06-17 20:46:21 +0900

    초기화

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,23 @@
+import styled, { ThemeProvider } from "styled-components/native";
 import { Slot } from "expo-router";
+import { darkTheme, lightTheme } from "@/constants/theme";
+import { useThemeStore } from "@/stores/theme/useThemeStore";
 
 function RootLayout() {
-    return <Slot />;
+    const { theme } = useThemeStore();
+
+    return (
+        <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+            <RootContainer>
+                <Slot />
+            </RootContainer>
+        </ThemeProvider>
+    );
 }
 
 export default RootLayout;
+
+const RootContainer = styled.View`
+    flex: 1;
+    background-color: ${props => props.theme.colors.background.default};
+`;
